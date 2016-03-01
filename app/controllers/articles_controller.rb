3b01bded4f2f766d5950909ca57324e29db68c1c
josh571 <joshnachbar@gmail.com>
2016-02-29 22:32:27 -0500

    add bootstrap styling to forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully"
+      flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
     else
       render 'new' # or render :new
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update 
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was succesfully updated!"
+      flash[:success] = "Article was succesfully updated!"
       redirect_to article_path(@article)
     else
       render 'edit' # or render :edit
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     set_article
     @article.destroy
-    flash[:notice] = "Article was successfully deleteddddddddd"
+    flash[:danger] = "Article was successfully deleteddddddddd"
     redirect_to articles_path
   end
 
