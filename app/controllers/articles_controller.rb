66107b2424e8cf63ba2af61658b8de06d0857a8a
Josh N <josh571@users.noreply.github.com>
2016-04-11 11:37:09 -0400

    update current user to be article author

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
